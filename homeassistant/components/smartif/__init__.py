bc77815d5849818f6abfc955e61018c6861a8a5e
Rui Eugénio <ruieugenio@gmail.com>
2023-05-04 01:47:54 +0100

    Replaced hass.config_entries.async_setup_platforms with await hass.config_entries.async_forward_entry_setups

--- homeassistant/components/smartif/__init__.py
+++ homeassistant/components/smartif/__init__.py
@@ -72,7 +72,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         coordinator=coordinator, client=smartif, all_services=all_services
     )
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
